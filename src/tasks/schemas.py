2a6b93260cdf0c87328e576525cd15afdfac8400
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-27 17:22:06 +0800

    Update schemas.py

--- src/tasks/schemas.py
+++ src/tasks/schemas.py
@@ -1,1 +1,17 @@
+from pydantic import BaseModel
+class TaskCreate(BaseModel):
+    description: str
+    dog_id: int
+
+class TaskUpdate(BaseModel):
+    status: str
+class TaskResponse(BaseModel):
+    id: int
+    description: str
+    status: str
+    dog_id: int
+    user_id: int
+
+    class Config:
+        from_attributes = True
 
