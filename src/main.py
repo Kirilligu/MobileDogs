276730a89d520363c74dd82fdc1c2205ef12f9e0
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-27 17:26:01 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,12 +4,8 @@ from .users.router import router as users_router
 from .devices.router import router as devices_router
 from .tasks.router import router as tasks_router
 
-# Создание базы данных
 Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
-
-# Регистрация роутеров
 app.include_router(users_router, prefix="/users", tags=["users"])
 app.include_router(devices_router, prefix="/devices", tags=["devices"])
 app.include_router(tasks_router, prefix="/tasks", tags=["tasks"])
