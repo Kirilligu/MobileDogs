1f21b83400d3d48715d0cdbf04936614de3feb81
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-27 17:27:30 +0800

    Update test_users.py

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,1 +1,14 @@
+from fastapi.testclient import TestClient
+from src.main import app
 
+client = TestClient(app)
+
+def test_register_user():
+    response = client.post("/users/register", json={
+        "first_name": "Kirill",
+        "last_name": "Deg",
+        "phone_number": "0987654321",
+        "password": "supersecret"
+    })
+    assert response.status_code == 200
+    assert response.json()["first_name"] == "Kirirll"
