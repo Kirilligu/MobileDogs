bd5231c20a81216945527e5ae203f7cd377145ee
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-06-09 13:48:12 +0800

    Update logging_config.py

--- src/logging_config.py
+++ src/logging_config.py
@@ -1,6 +1,8 @@
 import logging
 import sys
 import socket
+from datetime import datetime
+from elasticsearch import Elasticsearch
 
 hostname = socket.gethostname()
 
@@ -14,6 +16,7 @@ device_logger.setLevel(logging.DEBUG)
 
 file_handler = logging.FileHandler("logs/app.log")
 console_handler = logging.StreamHandler(sys.stdout)
+
 formatter = logging.Formatter('%(asctime)s - {} - %(name)s - %(levelname)s - %(message)s'.format(hostname))
 
 file_handler.setFormatter(formatter)
@@ -27,3 +30,24 @@ user_logger.addHandler(console_handler)
 
 device_logger.addHandler(file_handler)
 device_logger.addHandler(console_handler)
+
+es = Elasticsearch(["http://localhost:9200"])
+
+class ElasticsearchHandler(logging.Handler):
+    def emit(self, record):
+        log_entry = self.format(record)
+        es.index(index="logs", body={
+            "@timestamp": datetime.utcnow().isoformat(),
+            "hostname": hostname,
+            "level": record.levelname,
+            "message": log_entry,
+            "logger": record.name
+        })
+
+es_handler = ElasticsearchHandler()
+es_handler.setLevel(logging.DEBUG)
+es_handler.setFormatter(formatter)
+
+app_logger.addHandler(es_handler)
+user_logger.addHandler(es_handler)
+device_logger.addHandler(es_handler)
