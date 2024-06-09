04d8e98351cb589b864c938ad396980ab07b2aa3
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-06-09 13:43:19 +0800

    Update schemas.py

--- src/devices/schemas.py
+++ src/devices/schemas.py
@@ -1,22 +1,27 @@
 from pydantic import BaseModel
+from typing import Optional
+
 class CollarCreate(BaseModel):
     unique_number: str
     characteristics: str
+
 class DogCreate(BaseModel):
     name: str
     description: str
+
 class DogResponse(BaseModel):
     id: int
     name: str
     description: str
-    collar_id: int
+    collar_id: Optional[int] = None  # Обновление: сделать это поле необязательным
 
     class Config:
         from_attributes = True
+
 class CollarResponse(BaseModel):
     id: int
     unique_number: str
     characteristics: str
+
     class Config:
         from_attributes = True
-
